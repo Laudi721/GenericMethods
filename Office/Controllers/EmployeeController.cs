3de883dba6f8813a6ead2175ef20c0ed03b22bbe
Mateusz Leśniak <mlesniak@APLIX37>
2023-03-28 14:02:59 +0200

    rozpoczecie akcji pracownicy

---
+++ Office/Controllers/EmployeeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Office.Controllers
+{
+    public class EmployeeController : ControllerBase
+    {
+
+    }
+}
---
+++ Office/Interfaces/IEmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using Dtos.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Office.Interfaces
+{
+    public interface IEmployeeService
+    {
+        public IQueryable<EmployeeDto> Get();
+
+        public bool Post([FromBody] EmployeeDto employeeDto);
+
+        public bool Delete(int id);
+
+        public bool Put([FromBody] EmployeeDto employeeDto);
+    }
+}
